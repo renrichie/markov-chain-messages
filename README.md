6536a139cb2d001e493d8eb64d259ee52ae43770
Richie Ren <Richie Ren>
2017-05-23 16:17:39 -0700

    Added first version of algorithm; Updated README

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # markov-chain-messages
-Generates text-based messages using markov chaining on the given input
+Generates text-based messages using markov chaining on the given input.
+Inspired by reddit.com/r/subredditsimulator
+
+---------------------------WORK IN PROGRESS---------------------------
+TODO:
+Basics
+1. Finish the model
+2. Test the model
+3. Create a GUI
+4. Refactoring and polishing
+
+Planned feature
+- Implement usage of Twitter API to allow for scraping of user profile as input
--- src/model/MarkovChain.java
+++ src/model/MarkovChain.java
@@ -1,6 +1,10 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
+import org.apache.commons.math3.distribution.EnumeratedDistribution;
+import org.apache.commons.math3.util.Pair;
 
 /**
  * The fundamental structure to my implementation of the markov chain. 
@@ -48,10 +52,22 @@ public class MarkovChain {
 	
 	/**
 	 * Generates a response based on the possible states of the current word. 
-	 * The number of occurrences affects the chance of a specific state being chosen.
+	 * The number of occurrences affects the chance of a specific state being chosen. 
+	 * This is performed using a weighted selection via probability mass function. 
+	 * Requires the ApacheCommons math library from http://commons.apache.org/proper/commons-math/index.html.
 	 * @return a String that is one of many possible responses to the current word
 	 */
 	public String getPossibleState() {
-		return "TODO";
+		ArrayList<Pair<String,Double>> possibleStatesList = new ArrayList<>();
+		
+		// Adds all the states into an array list
+		Set<String> keys = possibleStates.keySet();
+		for (String key : keys) {
+			possibleStatesList.add(new Pair<String, Double>(key, 0.0 + possibleStates.get(key)));
+		}
+		
+		EnumeratedDistribution<String> weightedSelection = new EnumeratedDistribution<>(possibleStatesList);
+		
+		return weightedSelection.sample();
 	}
 }
